33bb0e97b82b45dc608e250efa50bc4c785925b8
Megan Bryan <meganmbryan12@gmail.com>
2021-03-28 16:39:38 -0700

    Add page to display employee list

---
+++ src/pages/List.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HeadingRow from "../components/HeadingRow";
+import EmployeeRow from "../components/EmployeeRow";
+
+function List() {
+    return (
+        <div>
+            <HeadingRow />
+            <EmployeeRow />
+        </div>
+    );
+}
+
+export default List;
