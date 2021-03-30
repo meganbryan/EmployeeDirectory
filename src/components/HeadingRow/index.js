6af726e87536d896665d3951dbdaf653606010c2
Megan Bryan <meganmbryan12@gmail.com>
2021-03-30 12:21:12 -0700

    Change styling of heading line

--- src/components/HeadingRow/index.js
+++ src/components/HeadingRow/index.js
@@ -3,19 +3,19 @@ import React from "react";
 function HeadingRow() {
     return (
         <div className="row mx-2 mt-2 px-2 pt-2 text-center">
-            <div className="col border p-2">
+            <div className="col border p-2 bg-dark text-white">
                 <strong>Last Name:</strong>
             </div>
-            <div className="col border p-2">
+            <div className="col border p-2 bg-dark text-white">
                 <strong>First Name:</strong>
             </div>
-            <div className="col border p-2">
+            <div className="col border p-2 bg-dark text-white">
                 <strong>Location:</strong>
             </div>
-            <div className="col border p-2">
-                <strong>Age:</strong>
+            <div className="col border p-2 bg-dark text-white">
+                <strong>State:</strong>
             </div>
-            <div className="col border p-2">
+            <div className="col border p-2 bg-dark text-white">
                 <strong>Email:</strong>
             </div>
         </div>
