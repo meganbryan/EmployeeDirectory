e69407ff4dd0ba6b1ad21df7126745a64bd5e9fb
Megan Bryan <meganmbryan12@gmail.com>
2021-03-30 10:36:41 -0700

    Add ability to call API and display columns

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import API from "../utils/API";
 import HeadingRow from "../components/HeadingRow";
 import EmployeeRow from "../components/EmployeeRow";
 
-function List() {
-    return (
-        <div>
-            <HeadingRow />
-            <EmployeeRow />
-        </div>
-    );
+class List extends Component {
+    state = {
+        lastname: "",
+        firstname: "",
+        location: "",
+        age: "",
+        email: ""
+    };
+
+    componentDidMount() {
+        this.retrieveEmployees();
+    }
+
+    retrieveEmployees = () => {
+        API.getEmployee()
+            .then(res =>
+                this.setState({
+                    lastname: res.name.last, 
+                    firstname: res.name.first,
+                    location: res.location,
+                    age: res.dob.age,
+                    email: res.email,
+                })
+            )
+            .catch(err => console.log(err));
+    };
+
+    render() {
+        return (
+            <div>
+                <HeadingRow />
+                <EmployeeRow lastname={this.state.lastname}/>
+            </div>
+        );
+    };
 }
 
 export default List;
