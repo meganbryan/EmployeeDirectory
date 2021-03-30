1db14931c605e7b9aa405f6a760cb59673c7737a
Megan Bryan <meganmbryan12@gmail.com>
2021-03-30 12:17:57 -0700

    Change API call to retrieve 50 results from US only

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const count = 50
 // eslint-disable-next-line 
 export default {
   getEmployee: function() {
-    return axios.get("https://randomuser.me/api/");
+    return axios.get(`https://randomuser.me/api/?results=${count}&nat=us`);
   }
 };
