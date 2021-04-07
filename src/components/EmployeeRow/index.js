a6ceade696bd91e722a74f93380669313363ec95
Megan Bryan <meganmbryan12@gmail.com>
2021-04-06 19:34:08 -0700

    Change size of columns

--- src/components/EmployeeRow/index.js
+++ src/components/EmployeeRow/index.js
@@ -3,19 +3,19 @@ import React from "react";
 function EmployeeRow({ lastname, firstname, city, state, email }) {
     return (
         <div className="row mx-3 text-center">
-            <div className="col border p-2">
+            <div className="col-2 border p-2">
                 {lastname}
             </div>
-            <div className="col border p-2">
+            <div className="col-2 border p-2">
                 {firstname}
             </div>
-            <div className="col border p-2">
+            <div className="col-2 border p-2">
                 {city}
             </div>
-            <div className="col border p-2">
+            <div className="col-2 border p-2">
                 {state}
             </div>
-            <div className="col border p-2">
+            <div className="col-4 border p-2">
                 {email}
             </div>
         </div>
